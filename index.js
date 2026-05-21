e48a190613beb73e4717749d251341e376807ae3
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-05-21 17:35:55 +0600

    add car api done

--- index.js
+++ index.js
@@ -22,12 +22,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    const db = client.db("driverfleetDB")
+    const carCollection = db.collection("cars");
+
+
+    app.post("/car", async (req, res) => {
+        const carData = req.body;
+        console.log(carData)
+        const result = await carCollection.insertOne(carData);
+        res.json(result);
+    })
     await client.db("driverfleetDB").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);

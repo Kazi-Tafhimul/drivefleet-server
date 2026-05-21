96e7cf3c55e8c7730246b6fa4baf096bbdaee023
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-05-21 13:16:34 +0600

    server setup done

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("server is running");
+})
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
+
